ad0a08a4155180bfc0289fd9794194c26ea1cc96
Ariful Islam <ariful.cse.bd@gmail.com>
2020-12-26 19:26:39 +0600

    Add project files.

---
+++ Web/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly DatabaseRepository _db = new DatabaseRepository();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var students = _db.GetAll();
+            return View(students);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                 var isSaved = _db.Add(student);
+
+                if (isSaved)
+                {
+                    ViewBag.Message = "Saved Successfully!";
+                }
+                else
+                {
+                    ViewBag.Message = "Saved Failed!";
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Error", "Something went wrong!");
+            }
+
+            return View(student);
+        }
+    }
+}
---
+++ Web/Models/DatabaseRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Web.Models
+{
+    public class DatabaseRepository
+    {
+        private readonly SqlConnection _sqlConnection = new SqlConnection(@"Server=.;Database=shihab;User Id=sa;Password=sa1234;");
+
+        private DataTable _dataTable;
+        private SqlDataAdapter _dataAdapter;
+
+        public List<Student> GetAll()
+        {
+            _sqlConnection.Open();
+
+            _dataTable = new DataTable();
+            _dataAdapter = new SqlDataAdapter();
+
+            using (var cmd = new SqlCommand("select * from students", _sqlConnection))
+            {
+                _dataAdapter.SelectCommand = cmd;
+                _dataAdapter.Fill(_dataTable);
+            }
+
+            _sqlConnection.Close();
+            _dataAdapter.Dispose();
+            _sqlConnection.Dispose();
+
+            return DataTableToList(_dataTable);
+        }
+
+        public List<Student> DataTableToList(DataTable dt)
+        {
+            var list = new List<Student>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                var student = new Student { 
+                    FirstName = row["FirstName"].ToString(), 
+                    LastName = row["LastName"].ToString() 
+                };
+
+                list.Add(student);
+            }
+
+            return list;
+        }
+
+        public bool Add(Student model)
+        {
+            _sqlConnection.Open();
+
+            var _query = $"INSERT INTO [dbo].[students]([FirstName],[LastName]) VALUES ('{model.FirstName}','{model.LastName}'";
+
+            var count = 0;
+            using (var cmd = new SqlCommand(_query, _sqlConnection))
+            {
+                count = cmd.ExecuteNonQuery();
+            }
+
+            _sqlConnection.Close();
+            _sqlConnection.Dispose();
+
+            return count > 0;
+        }
+
+        //T Get(int id) { }
+        //bool Delete(int id) { }
+        //void Update(T model) { }
+    }
+}
